5c9cc6766118dd8467d4f10a8a8844c3ae8e3fe0
amilahaque <amilahaque@gmail.com>
2024-10-07 12:38:55 -0500

    added command-line arguments to sqlite upload

--- my-project/upload/upload_to_SQLite.py
+++ my-project/upload/upload_to_SQLite.py
@@ -1,18 +1,34 @@
+# How to run: python upload_to_SQLite.py <path_to_csv_file> <database_name> <table_name>
+# Ex: python upload_to_SQLite.py ../../5g_core_data.csv demo.db fiveg_data
+
 import sqlite3
 import pandas as pd
+import argparse
 
-#Load data file 
-df = pd.read_csv('5g_core_data.csv')
+# Set up command-line argument parsing
+parser = argparse.ArgumentParser(description="Upload CSV to SQLite Database")
 
-#Data Clean Up
+# Add arguments for file name, database name, and table name
+parser.add_argument('csv_file', type=str, help="Path to the CSV file to be uploaded")
+parser.add_argument('database_name', type=str, help="Name of the SQLite database file")
+parser.add_argument('table_name', type=str, help="Name of the table to store the data")
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Load data file
+df = pd.read_csv(args.csv_file)
+
+# Data Clean Up: Strip whitespace from column names
 df.columns = df.columns.str.strip()
 
-#Create/connect to a SQLite database
-connection = sqlite3.connect('demo.db')
+# Create/connect to SQLite database
+connection = sqlite3.connect(args.database_name)
 
-#Load data file to SQLite
-#fail (will fail if file exists);replace;append
-df.to_sql('fiveg_data', connection, if_exists= 'replace', index=False)
+# Load data file to SQLite table (if_exists='replace' will replace the table if it exists)
+df.to_sql(args.table_name, connection, if_exists='replace', index=False)
 
-#Close connectiton
+# Close connection
 connection.close()
+
+print(f"Data from {args.csv_file} has been successfully uploaded to the '{args.table_name}' table in {args.database_name}")
