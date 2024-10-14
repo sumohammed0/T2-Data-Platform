03ae12e34357d74d81b544a3f0bfa96e72a3093a
fatimakqq <96496894+fatimakqq@users.noreply.github.com>
2024-10-13 23:03:37 -0500

    Testing database creation

--- my-project/backend/app/main.py
+++ my-project/backend/app/main.py
@@ -11,21 +11,34 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Configure CORS
+# CORS middleware makes sure communication is working across front end port 5173 and backend port 8000
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Update this with your React app's URL
+    allow_origins=["http://localhost:5173"],  #React app's URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-@app.get("/databases", response_model=list[schemas.Database])
-def list_databases(db: Session = Depends(get_db)):
-    return db.query(models.Database).all()
 
+
+#csv File upload
 @app.post("/upload-csv")
 async def upload_csv(file: UploadFile = File(...)):
     contents = await file.read()
     df = pd.read_csv(io.StringIO(contents.decode('utf-8')))
     return {"data": df.to_dict(orient="records")}
+
+#databases
+@app.get("/databases", response_model=list[schemas.Database])
+def list_databases(db: Session = Depends(get_db)):
+    return db.query(models.Database).all()
+
+#create new database entry
+@app.post("/databases", response_model=schemas.Database)
+def create_database(database: schemas.DatabaseCreate, db: Session = Depends(get_db)):
+    db_database = models.Database(**database.dict())
+    db.add(db_database)
+    db.commit()
+    db.refresh(db_database)
+    return db_database
